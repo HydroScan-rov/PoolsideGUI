c9bcabb32ffe47ac371a6bae5fb2a8bba17d6596
Grisha Starykh <grishafv@gmail.com>
2023-08-04 18:48:22 +0300

    Gamepad koefs updated

--- Control/Gamepad.cpp
+++ Control/Gamepad.cpp
@@ -1,21 +1,19 @@
 #include "Gamepad.h"
 
 const Gamepad::control_axis Gamepad::axis_table[] = {
-        {sf::Joystick::V,       SET_MARCH,  -320},
-        {sf::Joystick::X,       SET_LAG,    -320},
-        {sf::Joystick::U,       SET_YAW,    80},
-        {sf::Joystick::Y,       SET_PITCH,  120},
+        {sf::Joystick::V,       SET_MARCH,  -1.06},
+        {sf::Joystick::X,       SET_LAG,    1.06},
+        {sf::Joystick::U,       SET_YAW,    0.31},
+        {sf::Joystick::Y,       SET_PITCH,  0.62},
         {sf::Joystick::PovY,    TILT,       1.27},
-        {sf::Joystick::R,       SET_DEPTH,  160},
-        {sf::Joystick::Z,       SET_DEPTH,  160},
+        {sf::Joystick::R,       SET_DEPTH,  1.64},
+        {sf::Joystick::Z,       SET_DEPTH,  1.36},
 };
 
 const Gamepad::control_buttons Gamepad::buttons_table[] = {
-        // {0, 3, GRAB,        127},
-        // {2, 1, GRAB_ROTATE, 127},
 };
 
-Gamepad::Gamepad(int updateTimeMs): ControlBase("Gamepad") {
+Gamepad::Gamepad(int updateTimeMs) : ControlBase("Gamepad") {
     update_timer = new QTimer(this);
     connect(update_timer, SIGNAL(timeout()), this, SLOT(updateDevice()));
     update_timer->start(updateTimeMs);
@@ -25,13 +23,17 @@ void Gamepad::updateDevice() {
     sf::Joystick::update();
 
     for (unsigned int i = 0; i < sizeof(axis_table) / sizeof(axis_table[0]) - 2; i++) {
-        sendAction(axis_table[i].action,
-            sf::Joystick::getAxisPosition(id, axis_table[i].axis) * axis_table[i].multiplier);
+        if (abs(sf::Joystick::getAxisPosition(id, axis_table[i].axis) * axis_table[i].multiplier) < 0.1) {
+            sendAction(axis_table[i].action, static_cast<float>(0));
+        } else {
+            sendAction(axis_table[i].action,
+                sf::Joystick::getAxisPosition(id, axis_table[i].axis) * axis_table[i].multiplier);
+        }
     }
 
     sendAction(SET_DEPTH,
-        (sf::Joystick::getAxisPosition(id, axis_table[5].axis) * axis_table[5].multiplier) -
-        (sf::Joystick::getAxisPosition(id, axis_table[6].axis) * axis_table[6].multiplier));
+        ((sf::Joystick::getAxisPosition(id, axis_table[5].axis) + 100) * axis_table[5].multiplier/2 -
+        ((sf::Joystick::getAxisPosition(id, axis_table[6].axis) + 100) * axis_table[6].multiplier/2)));
 
     for (unsigned int i = 0; i < sizeof(buttons_table) / sizeof(buttons_table[0]); i++) {
         if (sf::Joystick::isButtonPressed(id, buttons_table[i].button_id) and
